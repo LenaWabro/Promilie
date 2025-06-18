19f3e736af5b150ef495de784f425d91895d10bf
lenaw <s2310456027@fhooe.at>
2025-06-18 14:17:59 +0200

    test branches

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Servus</Text>
+      <Text>Servus hallo</Text>
       <StatusBar style="auto" />
     </View>
   );
