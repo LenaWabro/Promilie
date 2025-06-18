6b800a8f18f027c48eb857ce729f7ff1e2dd33cf
lenaw <s2310456027@fhooe.at>
2025-06-18 14:55:17 +0200

    test branches

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hallo und Willkommen auf unserer App</Text>
+      <Text>Servus</Text>
       <StatusBar style="auto" />
     </View>
   );
